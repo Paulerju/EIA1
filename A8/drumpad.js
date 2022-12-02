43595e4ccd01653322ad0dd603fd6c206e0673e9
Paulerju <101625370+Paulerju@users.noreply.github.com>
2022-12-02 16:24:19 +0100

    Drummpad

---
+++ A8/drumpad.js
@@ -0,0 +1,48 @@
+const A = new Audio('DrumPad/A.mp3');
+const B = new Audio('DrumPad/C.mp3');
+const C = new Audio('DrumPad/F.mp3');
+const D = new Audio('DrumPad/G.mp3');
+const E = new Audio('DrumPad/hihat.mp3');
+const F = new Audio('DrumPad/kick.mp3');
+const G = new Audio('DrumPad/laugh-1.mp3');
+const H = new Audio('DrumPad/laugh-2.mp3');
+const I = new Audio('DrumPad/snare.mp3');
+let Beat = [F, I, E];
+document.querySelector(".button1").addEventListener('click', function () {
+    playSample(A);
+});
+document.querySelector(".button2").addEventListener('click', function () {
+    playSample(B);
+});
+document.querySelector(".button3").addEventListener('click', function () {
+    playSample(C);
+});
+document.querySelector(".button4").addEventListener('click', function () {
+    playSample(D);
+});
+document.querySelector(".button5").addEventListener('click', function () {
+    playSample(E);
+});
+document.querySelector(".button6").addEventListener('click', function () {
+    playSample(F);
+});
+document.querySelector(".button7").addEventListener('click', function () {
+    playSample(G);
+});
+document.querySelector(".button8").addEventListener('click', function () {
+    playSample(H);
+});
+document.querySelector(".button9").addEventListener('click', function () {
+    playSample(I);
+});
+function playSample(var1) {
+    var1.play();
+}
+let i = 0;
+document.querySelector('.play').addEventListener('click', function () {
+    setInterval(function () {
+        Beat[i].play();
+        i++;
+    }, 500);
+});
+//# sourceMappingURL=drumpad.js.map
---
+++ A8/drumpad.ts
@@ -0,0 +1,58 @@
+const A:HTMLAudioElement = new Audio('DrumPad/A.mp3');
+const B:HTMLAudioElement = new Audio('DrumPad/C.mp3');
+const C:HTMLAudioElement = new Audio('DrumPad/F.mp3');
+const D:HTMLAudioElement = new Audio('DrumPad/G.mp3');
+const E:HTMLAudioElement = new Audio('DrumPad/hihat.mp3');
+const F:HTMLAudioElement = new Audio('DrumPad/kick.mp3');
+const G:HTMLAudioElement = new Audio('DrumPad/laugh-1.mp3');
+const H:HTMLAudioElement = new Audio('DrumPad/laugh-2.mp3');
+const I:HTMLAudioElement = new Audio('DrumPad/snare.mp3');
+let Beat:any= [F, I, E]; 
+
+
+document.querySelector(".button1").addEventListener('click', function() {
+    playSample(A);
+});
+document.querySelector(".button2").addEventListener('click', function() {
+    playSample(B);
+});
+document.querySelector(".button3").addEventListener('click', function() {
+    playSample(C);
+});
+document.querySelector(".button4").addEventListener('click', function() {
+    playSample(D);
+});
+document.querySelector(".button5").addEventListener('click', function() {
+    playSample(E);
+});
+document.querySelector(".button6").addEventListener('click', function() {
+    playSample(F);
+});
+document.querySelector(".button7").addEventListener('click', function() {
+    playSample(G);
+});
+document.querySelector(".button8").addEventListener('click', function() {
+    playSample(H);
+});
+document.querySelector(".button9").addEventListener('click', function() {
+    playSample(I);
+});
+
+
+function playSample(var1) {
+
+var1.play();
+
+}
+let i=0; 
+document.querySelector('.play').addEventListener('click', function() {
+    setInterval(function( ) {
+               Beat[i].play(); 
+               i++; 
+    }, 500);
+    
+});
+
+
+ 
+ 
